9ba15b643c44d245e68da8cdd9fab09356016425
JohnDeVone <54861937+JohnDeVone@users.noreply.github.com>
2019-09-03 14:34:45 -0400

    Using Turtle in Python to make 2 connected squares.

---
+++ P2HW2_TurtleGraphic_JohnDevone.py
@@ -0,0 +1,28 @@
+# In this assignment i will practice how to use libraries and draw on a canvas
+# Sept 9,2019
+# CTI-110 P2HW2 - Turtle Graphic
+# John DeVone
+
+# Import the TURTLE DARNIT
+import turtle
+
+# ONE square coming up HOT AND READY
+turtle.right(45)
+turtle.forward(50)
+turtle.left(90)
+turtle.forward(50)
+turtle.left(90)
+turtle.forward(50)
+turtle.left(90)
+turtle.forward(50)
+
+# Next square coming RIGHT on up!!!
+turtle.forward(50)
+turtle.right(90)
+turtle.forward(50)
+turtle.right(90)
+turtle.forward(50)
+turtle.right(90)
+turtle.forward(50)
+
+turtle.exitonclick()
